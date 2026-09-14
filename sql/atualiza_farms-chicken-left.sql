382d54707c7dbd308ecb2c0265d5a873cc8c36b4
Nicolas25vlad <nicolas25vlad@proton.me>
2026-09-14 11:34:26 -0300

    fix: explicitar restricao ao remover view lots

--- sql/atualiza_farms-chicken-left.sql
+++ sql/atualiza_farms-chicken-left.sql
@@ -6,7 +6,7 @@ ALTER TABLE farms
 ALTER TABLE farm_owners
     ADD COLUMN IF NOT EXISTS foto_url TEXT;
 
-DROP VIEW IF EXISTS midas.lots;
+DROP VIEW IF EXISTS midas.lots RESTRICT;
 
 ALTER TABLE lots
     DROP COLUMN IF EXISTS date_birth,
